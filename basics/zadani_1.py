eca39a67575372a4625ad60eed6536a1fc371e7f
Jusiek <rafajuskiewicz@gmail.com>
2019-06-16 09:27:24 +0200

    init

---
+++ basics/zadani_1.py
@@ -0,0 +1,6 @@
+x = input("Podaj pierwsza podstawe")
+y = input("podaj druga podstawe")
+z = input("podaj wysokosc")
+
+funk = ((x +y) * z /2 )
+print ("Wynik jest rowny" funk)
---
+++ basics/zadanie.py
@@ -0,0 +1,7 @@
+x = 11
+
+if x > 10:
+    print(x)
+    print(x + 1)
+elif x > 0:
+    print (f"0{x}")
